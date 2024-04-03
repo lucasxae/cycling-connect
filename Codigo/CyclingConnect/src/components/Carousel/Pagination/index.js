ae81c7ce7ad94f856c842399eee0bec2c76f0c3b
Ana Luiza <analuizaalvesm@gmail.com>
2024-04-03 15:47:12 -0300

    removido libs reanimated

--- Codigo/CyclingConnect/src/components/Carousel/Pagination/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Dimensions} from 'react-native';
-import * as S from './styles';
-
-function Pagination({data}) {
-  return <S.DotContainer></S.DotContainer>;
-}
-
-export default Pagination;
--- Codigo/CyclingConnect/src/components/index.js
+++ Codigo/CyclingConnect/src/components/index.js
@@ -1,5 +1,4 @@
 export {default as Button} from './Button';
-export {default as Carousel} from './Carousel';
 export {default as CustomInput} from './CustomInput';
 export {default as CustomText} from './CustomText';
 export {default as Separator} from './Separator';
