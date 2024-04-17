52eac2249dabc1d630279ddde48ee511f2ed3c4c
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2024-04-16 22:57:04 -0300

    Add ChangePassword screen

--- Codigo/CyclingConnect/src/screens/ChangePassword/index.js
+++ Codigo/CyclingConnect/src/screens/ChangePassword/index.js
@@ -1,14 +1,9 @@
-import React, {useCallback} from 'react';
+import React from 'react';
+import {View, Keyboard, Text} from 'react-native';
+import {CustomInput} from '../../components';
 import {useForm, Controller} from 'react-hook-form';
 import {zodResolver} from '@hookform/resolvers/zod';
-import {
-  Button,
-  CustomText,
-  CustomInput,
-  Separator,
-  Link,
-} from '../../components';
-import {Text} from 'react-native';
+import {resetPasswordSchema} from '../../utils/schemas/schemas';
 import * as S from './styles';
 
 function ChangePassword() {
@@ -22,16 +17,77 @@ function ChangePassword() {
       password: '',
       confirmPassword: '',
     },
+    resolver: zodResolver(resetPasswordSchema),
   });
 
+  const onSubmit = data => console.log(data);
+
   return (
-    <S.KeyboardWrapper>
-      <S.SafeAreaView>
+    <S.SafeAreaView>
+      <S.TouchableWrapper onPress={() => Keyboard.dismiss()}>
         <S.Container>
-          <Text>Alterar senha</Text>
+          <View>
+            <S.Header>
+              <S.Title>Alterar senha</S.Title>
+            </S.Header>
+            <Controller
+              name="password"
+              control={control}
+              render={({field: {onChange, onBlur, value}}) => (
+                <CustomInput
+                  label="Nova senha"
+                  placeholder="Nova senha"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  isPassword
+                  password
+                  value={value}
+                  minLength={8}
+                  maxLength={16}
+                />
+              )}
+            />
+            {errors.password && <S.Error>{errors.password.message}</S.Error>}
+            <Controller
+              name="confirmPassword"
+              control={control}
+              render={({field: {onChange, onBlur, value}}) => (
+                <CustomInput
+                  label="Confirmar senha"
+                  placeholder="Confirmar senha"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  isPassword
+                  password
+                  hasMargin
+                  value={value}
+                  minLength={8}
+                  maxLength={16}
+                />
+              )}
+            />
+            {errors.confirmPassword && (
+              <S.Error>{errors.confirmPassword.message}</S.Error>
+            )}
+            <S.BulletList>
+              <S.BulletItem>• Min. 8 caracteres</S.BulletItem>
+              <S.BulletItem>• Incluir 1 letra maiúscula e 1 minúscula</S.BulletItem>
+              <S.BulletItem>• Incluir um número</S.BulletItem>
+              <S.BulletItem>• Incluir um caractere especial</S.BulletItem>
+            </S.BulletList>
+          </View>
+          <View>
+            <S.CustomButton
+              fullWidth={true}
+              onPress={handleSubmit(onSubmit)}
+              bgColor={'#fff'}
+              disabled={!isValid || isSubmitting}>
+              <S.ButtonText>Continuar</S.ButtonText>
+            </S.CustomButton>
+          </View>
         </S.Container>
-      </S.SafeAreaView>
-    </S.KeyboardWrapper>
+      </S.TouchableWrapper>
+    </S.SafeAreaView>
   );
 }
 
--- Codigo/CyclingConnect/src/screens/ChangePassword/styles.js
+++ Codigo/CyclingConnect/src/screens/ChangePassword/styles.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components/native';
+import {Button} from '../../components';
+import {TouchableWithoutFeedback} from 'react-native';
 import theme from '../../global/theme';
 
 const {colors, fonts, fontSizes} = theme;
@@ -7,13 +9,59 @@ export const KeyboardWrapper = styled.KeyboardAvoidingView`
   flex: 1;
 `;
 
+export const TouchableWrapper = styled(TouchableWithoutFeedback).attrs({
+  flex: 1,
+  accessible: false,
+})``;
+
 export const SafeAreaView = styled.SafeAreaView`
   flex: 1;
-  background-color: ${colors.background.primary};
+  background-color: ${colors.secondary.background};
 `;
 
 export const Container = styled.View`
   flex: 1;
-  padding: 40px 30px 30px;
-  background-color: ${colors.background.primary};
+  padding: 0 30px 30px 30px;
+  justify-content: space-between;
+`;
+
+export const Header = styled.View`
+  margin-bottom: 30px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${fonts.secondary.secondarySemiBold};
+  font-size: ${fontSizes.extraLarge};
+  color: ${colors.redPalette.primary};
+`;
+
+export const BulletList = styled.View`
+  margin-top: 20px;
+  margin-left: 6px;
+`;
+
+export const BulletItem = styled.Text`
+  font-family: ${fonts.primary.primaryRegular};
+  font-size: ${fontSizes.small};
+  color: ${colors.secondary.lightText};
+  line-height: 18px;
+`;
+
+
+export const CustomButton = styled(Button).attrs({
+  bgColor: colors.redPalette.primary,
+})``;
+
+export const ButtonText = styled.Text`
+  font-family: ${fonts.primary.primaryBold};
+  font-size: ${fontSizes.regular};
+  color: ${colors.white};
+`;
+
+export const Error = styled.Text`
+  margin-top: 6px;
+  font-family: ${fonts.primary.primaryRegular};
+  font-size: ${fontSizes.smaller};
+  color: ${colors.error};
+  text-align: ${({align}) => (align ? align : 'left')};
 `;
